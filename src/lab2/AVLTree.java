77dbd2e978248d6354eca180ca8adda3c1baa706
Gavril <donetlucky242@gmail.com>
2023-03-14 21:44:45 +0200

    Homework ver2.0 homewrok lab4 - comparing ada to bst

--- src/lab2/AVLTree.java
+++ src/lab2/AVLTree.java
@@ -14,10 +14,10 @@ class Node {
     }
 }
 
-public  class AVLTree {
+public class AVLTree {
 
 
-    private static final int N = 100000;
+    private static final int N = 10000;
     Node root;
 
     private void updateBalanceFactor(Node node) {
@@ -30,16 +30,16 @@ public  class AVLTree {
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
-    private Node rightRotate(Node node) {
-        Node newRoot = node.left;
-        node.left = newRoot.right;
-        if (node.left != null)
-            node.left.parent = node;
-        newRoot.right = node;
-        newRoot.parent = node.parent;
-        node.parent = newRoot;
+    private Node rightRotate(Node Node) {
+        Node newRoot = Node.left;
+        Node.left = newRoot.right;
+        if (Node.left != null)
+            Node.left.parent = Node;
+        newRoot.right = Node;
+        newRoot.parent = Node.parent;
+        Node.parent = newRoot;
 
-        updateBalanceFactor(node);
+        updateBalanceFactor(Node);
         updateBalanceFactor(newRoot);
 
         return newRoot;
@@ -122,11 +122,18 @@ public  class AVLTree {
         if (node == null) {
             System.out.println("Key " + key + " not found");
         } else {
-            System.out.println("Search time for key " + key + ": " + (end - start) + " nanoseconds");
+            System.out.println("Search time for key " + key + ": " + (end - start) + " nanoseconds  for AVL");
         }
     }
 
     public static void main(String[] args) {
+
+        NodeBST treeBST = new NodeBST(8);
+       // System.out.println("high");
+        for (int i = 0  ;   i < 10 ; i++) { treeBST.insert(i);}
+       // System.out.println("low");
+
+        //treeBST.printLevels(treeBST);
         AVLTree tree = new AVLTree();
 
         // Insert N random keys
@@ -137,7 +144,19 @@ public  class AVLTree {
             tree.insert(key);
         }
         long end = System.nanoTime();
-        System.out.println("Insertion time for " + N + " random keys: " + (end - start)/1000000 + " miliseconds");
+        System.out.println("Insertion time for " + N + " random keys: " + (end - start)/1000000 + " miliseconds AVL");
+        //BST
+         start = System.nanoTime();
+         random = new Random();
+        for (int i = 0; i < N; i++) {
+            int key = random.nextInt(N);
+            treeBST.insert(key);
+        }
+         end = System.nanoTime();
+        System.out.println("Insertion time for " + N + " random keys: " + (end - start)/1000000 + " miliseconds BST");
+
+        AVLTree.searchTime(tree,1000);
+        treeBST.searchTime(treeBST,1000);
 
         tree = new AVLTree();
 
@@ -147,49 +166,97 @@ public  class AVLTree {
             tree.insert(i);
         }
         end = System.nanoTime();
-        System.out.println("Insertion time for " + N + " keys in increasing order: " + (end - start)/1000000 + " miliseconds");
+        System.out.println("Insertion time for " + N + " keys in increasing order: " + (end - start)/1000000 + " miliseconds AVL");
+        treeBST = new NodeBST(0);
+        start = System.nanoTime();
+        for (int i = 0; i < N; i++) {
+            treeBST.insert(i);
+        }
+        end = System.nanoTime();
+        System.out.println("Insertion time for " + N + " keys in increasing order: " + (end - start)/1000000 + " miliseconds BST");
 
-        AVLTree.searchTime(tree,100);
+        AVLTree.searchTime(tree,1000);
+        treeBST.searchTime(treeBST,1000);
     }
 
 
 }
 
 
-class BST_class {
+class NodeBST {
+        int key, balanceleft, balanceright;
+        NodeBST left, right, parent;
 
-    public static int height = 0;
-
-    public static class Node {
-        int key,balanceleft,balanceright;
-        Node left, right, parent;
-
-        Node(int key) {
+        public NodeBST(int key) {
             this.key = key;
             left = null;
             right = null;
             parent = null;
         }
 
+        public void insert(int key) {
+            if (key < this.key) {
+                if (left == null) {
+                    left = new NodeBST(key);
+                    left.parent = this;
+                } else {
+                    left.insert(key);
+                }
+            } else {
+                if (right == null) {
+                    right = new NodeBST(key);
+                    right.parent = this;
+                } else {
+                    right.insert(key);
+                }
+            }
+        }
+    public NodeBST search(NodeBST root,int  key) {
+        NodeBST node = root;
+        while (node != null) {
+            if (key == node.key) {
+                return node;
+            } else if (key < node.key) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return null;
+    }
 
-        public static void printLeafs(Node node) { //optional
-            if (node == null) {
+
+    public static void searchTime(NodeBST tree, int key) {
+        long start = System.nanoTime();
+        NodeBST node = tree.search(tree,key);
+        long end = System.nanoTime();
+        if (node == null) {
+            System.out.println("Key " + key + " not found");
+        } else {
+            System.out.println("Search time for key " + key + ": " + (end - start) + " nanoseconds BST");
+        }
+    }
+
+
+
+    public static void printLeafs(NodeBST nodeBST) { //optional
+            if (nodeBST == null) {
                 return;
             }
-            if (node.left == null && node.right == null) {
-                System.out.println(node.key);
+            if (nodeBST.left == null && nodeBST.right == null) {
+                System.out.println(nodeBST.key);
             } else {
-                printLeafs(node.left);
-                printLeafs(node.right);
+                printLeafs(nodeBST.left);
+                printLeafs(nodeBST.right);
             }
         }
 
-        private int isBalancedNode(Node node) {
-            if (node == null) {
+        private int isBalancedNode(NodeBST nodeBST) {
+            if (nodeBST == null) {
                 return 0;
             }
-            int leftHeight = isBalancedNode(node.left);
-            int rightHeight = isBalancedNode(node.right);
+            int leftHeight = isBalancedNode(nodeBST.left);
+            int rightHeight = isBalancedNode(nodeBST.right);
             if (leftHeight < 0 || rightHeight < 0 || Math.abs(leftHeight - rightHeight) > 1) {
                 return -1;
             }
@@ -199,30 +266,30 @@ class BST_class {
 
 
         //  ive used Math.abs because the result sometimes was negative meaning its not the closest node then.
-        private static int searchClosestNode(Node node, int key) {
+        private static int searchClosestNode(NodeBST nodeBST, int key) {
             int closest = 0;
-            if (node == null) {
+            if (nodeBST == null) {
                 return closest;
             }
-            if (node.key == key) {
+            if (nodeBST.key == key) {
                 return key;
-            } else if (key < node.key) {
-                if (Math.abs(node.key - key) < Math.abs(closest - key)) {
-                    closest = node.key;
+            } else if (key < nodeBST.key) {
+                if (Math.abs(nodeBST.key - key) < Math.abs(closest - key)) {
+                    closest = nodeBST.key;
                 }
-                return searchClosestNode(node.left, key);
+                return searchClosestNode(nodeBST.left, key);
             } else {
-                if (Math.abs(node.key - key) < Math.abs(closest - key)) {
-                    closest = node.key;
+                if (Math.abs(nodeBST.key - key) < Math.abs(closest - key)) {
+                    closest = nodeBST.key;
                 }
-                return searchClosestNode(node.right, key);
+                return searchClosestNode(nodeBST.right, key);
             }
         }
 
-        public static boolean checkExistTwoNodesWithSum(Node root, int s) {
+        public static boolean checkExistTwoNodesWithSum(NodeBST root, int s) {
             List<Integer> values = new ArrayList<>();
-            Stack<Node> stack = new Stack<>();
-            Node current = root;
+            Stack<NodeBST> stack = new Stack<>();
+            NodeBST current = root;
 
             while (current != null || !stack.isEmpty()) {
                 while (current != null) {
@@ -250,60 +317,60 @@ class BST_class {
         }
 
 
-        public static void printLevels(Node node) {  // ex 6
-            if (node == null) {
+        public static void printLevels(NodeBST nodeBST) {  // ex 6
+            if (nodeBST == null) {
                 return;
             }
 
-            Queue<Node> queue = new LinkedList<>(); // we add them in order in a queue recursively and then print them;
-            queue.add(node);
+            Queue<NodeBST> queue = new LinkedList<>(); // we add them in order in a queue recursively and then print them;
+            queue.add(nodeBST);
 
             while (!queue.isEmpty()) {
                 int levelSize = queue.size();
                 System.out.println("");
 
                 for (int i = 0; i < levelSize; i++) {
-                    Node currNode = queue.poll();
-                    System.out.print(currNode.key + " ");
+                    NodeBST currNodeBST = queue.poll();
+                    System.out.print(currNodeBST.key + " ");
 
-                    if (currNode.left != null) {
-                        queue.add(currNode.left);
+                    if (currNodeBST.left != null) {
+                        queue.add(currNodeBST.left);
                     }
-                    if (currNode.right != null) {
-                        queue.add(currNode.right);
+                    if (currNodeBST.right != null) {
+                        queue.add(currNodeBST.right);
                     }
                 }
             }
         }
 
-        public static void printPathFromTo(Node root, Node node1, Node node2) {
-            Node lca = lowestCommonAncestor(root, node1, node2);
+        public static void printPathFromTo(NodeBST root, NodeBST nodeBST1, NodeBST nodeBST2) {
+            NodeBST lca = lowestCommonAncestor(root, nodeBST1, nodeBST2);
             if (lca == null) {
-                System.out.println("No path exists between " + node1.key + " and " + node2.key);
+                System.out.println("No path exists between " + nodeBST1.key + " and " + nodeBST2.key);
                 return;
             }
-            List<Node> path1 = new ArrayList<>();
-            getPath(lca, node1, path1);
-            List<Node> path2 = new ArrayList<>();
-            getPath(lca, node2, path2);
+            List<NodeBST> path1 = new ArrayList<>();
+            getPath(lca, nodeBST1, path1);
+            List<NodeBST> path2 = new ArrayList<>();
+            getPath(lca, nodeBST2, path2);
             Collections.reverse(path1);
-            for (Node node : path1) {
-                System.out.print(node.key + " ");
+            for (NodeBST nodeBST : path1) {
+                System.out.print(nodeBST.key + " ");
             }
-            for (Node node : path2) {
-                System.out.print(node.key + " ");
+            for (NodeBST nodeBST : path2) {
+                System.out.print(nodeBST.key + " ");
             }
         }
 
-        private static Node lowestCommonAncestor(Node root, Node node1, Node node2) {
+        private static NodeBST lowestCommonAncestor(NodeBST root, NodeBST nodeBST1, NodeBST nodeBST2) {
             if (root == null) {
                 return null;
             }
-            if (root == node1 || root == node2) {
+            if (root == nodeBST1 || root == nodeBST2) {
                 return root;
             }
-            Node left = lowestCommonAncestor(root.left, node1, node2);
-            Node right = lowestCommonAncestor(root.right, node1, node2);
+            NodeBST left = lowestCommonAncestor(root.left, nodeBST1, nodeBST2);
+            NodeBST right = lowestCommonAncestor(root.right, nodeBST1, nodeBST2);
             if (left != null && right != null) {
                 return root;
             } else if (left != null) {
@@ -313,15 +380,15 @@ class BST_class {
             }
         }
 
-        private static boolean getPath(Node root, Node node, List<Node> path) {
+        private static boolean getPath(NodeBST root, NodeBST nodeBST, List<NodeBST> path) {
             if (root == null) {
                 return false;
             }
             path.add(root);
-            if (root == node) {
+            if (root == nodeBST) {
                 return true;
             }
-            if (getPath(root.left, node, path) || getPath(root.right, node, path)) {
+            if (getPath(root.left, nodeBST, path) || getPath(root.right, nodeBST, path)) {
                 return true;
             }
             path.remove(path.size() - 1);
@@ -330,101 +397,99 @@ class BST_class {
 
 //---------------------------------------------
 
-        public static void printPathsWithSum(Node root, int s) {
+        public static void printPathsWithSum(NodeBST root, int s) {
             List<Integer> path = new ArrayList<>();
             printPathsWithSumHelper(root, s, path, 0);
         }
 
-        private static void printPathsWithSumHelper(Node node, int s, List<Integer> path, int sumSoFar) {
-            if (node == null) {
+        private static void printPathsWithSumHelper(NodeBST nodeBST, int s, List<Integer> path, int sumSoFar) {
+            if (nodeBST == null) {
                 return;
             }
 
-            sumSoFar += node.key;
-            path.add(node.key);
+            sumSoFar += nodeBST.key;
+            path.add(nodeBST.key);
 
-            if (sumSoFar == s && node.left == null && node.right == null) {
+            if (sumSoFar == s && nodeBST.left == null && nodeBST.right == null) {
                 // Print the path
                 for (int i = 0; i < path.size(); i++) {
-                    if(path.get(i)!=s) //i get a bug where it shows the node itself many times
-                    System.out.print(path.get(i) + " ");
+                    if (path.get(i) != s) //i get a bug where it shows the node itself many times
+                        System.out.print(path.get(i) + " ");
                 }
                 System.out.println();
             }
 
-            printPathsWithSumHelper(node.left, s, path, sumSoFar);
-            printPathsWithSumHelper(node.right, s, path, sumSoFar);
+            printPathsWithSumHelper(nodeBST.left, s, path, sumSoFar);
+            printPathsWithSumHelper(nodeBST.right, s, path, sumSoFar);
 
             // Backtrack
             path.remove(path.size() - 1);
-            printPathsWithSumHelper(node.left, s, new ArrayList<>(), 0);
-            printPathsWithSumHelper(node.right, s, new ArrayList<>(), 0);
+            printPathsWithSumHelper(nodeBST.left, s, new ArrayList<>(), 0);
+            printPathsWithSumHelper(nodeBST.right, s, new ArrayList<>(), 0);
         }
 
-    }
 
 //-------------------------------------------------------------------
 
 
-    //print leafs, tree, add , remove, optimize and more ----- tasks
-    public static void main(String[] args) {
+        //print leafs, tree, add , remove, optimize and more ----- tasks
+        public static void main(String[] args) {
 
-        //we can replace this mess with a simple add method
-        Node root = new Node(8);
-        root.left = new Node(2);
-        root.right = new Node(15);
-        root.left.left = new Node(1);
-        root.left.right = new Node(5);
-        root.right.left = new Node(10);
-        root.right.right = new Node(20);
-        root.left.right.left = new Node(4);
-        root.left.right.right = new Node(7);
-        root.right.right.left = new Node(18);
-        root.right.right.right = new Node(22);
-        root.left.right.left.left = new Node(3);
+            //we can replace this mess with a simple add method
+            NodeBST root = new NodeBST(8);
+            root.left = new NodeBST(2);
+            root.right = new NodeBST(15);
+            root.left.left = new NodeBST(1);
+            root.left.right = new NodeBST(5);
+            root.right.left = new NodeBST(10);
+            root.right.right = new NodeBST(20);
+            root.left.right.left = new NodeBST(4);
+            root.left.right.right = new NodeBST(7);
+            root.right.right.left = new NodeBST(18);
+            root.right.right.right = new NodeBST(22);
+            root.left.right.left.left = new NodeBST(3);
 
-        System.out.println("Leaf nodes:");
-        BST_class.Node.printLeafs(root);
+            System.out.println("Leaf nodes:");
+            NodeBST.printLeafs(root);
 
-        // testing isBalancedNode method
-        int result = root.isBalancedNode(root);
-        if (result >= 0) {
-            System.out.println("Tree is balanced" + result);
-        } else {
-            System.out.println("Tree is not balanced"+ result);
+            // testing isBalancedNode method
+            int result = root.isBalancedNode(root);
+            if (result >= 0) {
+                System.out.println("Tree is balanced" + result);
+            } else {
+                System.out.println("Tree is not balanced" + result);
+            }
+
+            // testing searchClosestNode method for  7
+            int closestNode = NodeBST.searchClosestNode(root, 7);
+            System.out.println("Closest node to 7 is: " + closestNode);
+
+            // testing checkExistTwoNodesWithSum method
+            boolean result2 = NodeBST.checkExistTwoNodesWithSum(root, 21);
+            if (result2) {
+                System.out.println("There are two nodes with sum 21");
+            } else {
+                System.out.println("There are no two nodes with sum 21");
+            }
+
+            // test printLevels method
+            System.out.println("Nodes by level:");
+            NodeBST.printLevels(root);
+
+            // test printPathFromTo
+
+            System.out.println("\nPath from 1 to 20:");
+            NodeBST.printPathFromTo(root, root.left.left, root.right.right);
+
+            // test printPathsWithSum method
+            System.out.println("\nPaths with sum 22:");
+
+            NodeBST.printPathsWithSum(root, 22);
+
+
         }
-
-        // testing searchClosestNode method for  7
-        int closestNode = BST_class.Node.searchClosestNode(root, 7);
-        System.out.println("Closest node to 7 is: " + closestNode);
-
-        // testing checkExistTwoNodesWithSum method
-        boolean result2 = BST_class.Node.checkExistTwoNodesWithSum(root, 21);
-        if (result2) {
-            System.out.println("There are two nodes with sum 21");
-        } else {
-            System.out.println("There are no two nodes with sum 21");
-        }
-
-        // test printLevels method
-        System.out.println("Nodes by level:");
-        BST_class.Node.printLevels(root);
-
-        // test printPathFromTo
-
-        System.out.println("\nPath from 1 to 20:");
-        BST_class.Node.printPathFromTo(root, root.left.left, root.right.right);
-
-        // test printPathsWithSum method
-        System.out.println("\nPaths with sum 22:");
-
-        BST_class.Node.printPathsWithSum(root, 22);
 
 
     }
-
-
-}
-
 
 
